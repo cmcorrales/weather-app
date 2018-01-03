b5c06d174448b496cf47edeb303701e237e2ee24
Cassandra <cmcorrales3@gmail.com>
2018-01-03 06:26:00 -0500

    add toggle switch

--- app.js
+++ app.js
@@ -2,15 +2,22 @@ $(document).ready(function() {
   var APIKEY = "5145288f4fd76b9332393c30382a8524"
   function getWeather(latitude, longitude) {
     $.getJSON('http://api.openweathermap.org/data/2.5/weather?lat='+latitude+'&lon='+longitude+'&units=imperial&APPID='+APIKEY+'', function(data) {
-    $("#city").html(data.name)
-    $("#temperature").html(data.main.temp+" &deg;C")
-    $("#icon").html('<img src="http://openweathermap.org/img/w/'+data.weather[0].icon+'.png">')
+    var city = data.name;
+    var temperature = data.main.temp;
+    var icon = data.weather[0].icon
+    $("#city").html(city)
+    $("#temperature").html(temperature+" &deg;F")
+    $("#icon").html('<img src="http://openweathermap.org/img/w/'+icon+'.png">')
+    $(".switch").click(function() {
+      console.log("click");
+
+    })
   })
   }
   navigator.geolocation.getCurrentPosition(function(position) {
     getWeather(position.coords.latitude, position.coords.longitude)
   });
   function geo_error() {
-    alert("Sorry, no position available.");
+    alert("Sorry, no position available, cannot determine weather.");
   }
 })
